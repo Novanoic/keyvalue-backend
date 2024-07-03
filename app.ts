27b65b9e4c41e45bb58dd05d0979f4bb5351a8b3
Nalin Govind V <nalingovind@gmail.com>
2024-07-03 22:51:17 +0530

    A database created and linked with app.js

--- app.ts
+++ app.ts
@@ -1,37 +1,28 @@
 const express = require("express");
 import { Request, Response } from "express";
+import employeeRouter from "./employeeRouter";
+import loggerMiddleware from "./loggerMiddleware";
+import bodyParser from "body-parser";
+import dataSource from "./data-source";
+
 const server = new express();
 
+server.use(bodyParser.json());
+server.use(loggerMiddleware);
+server.use("/employees", employeeRouter);
+
 server.get("/", (req: Request, res: Response) => {
-  console.log(req.url);
   res.status(200).send("Hello World");
 });
 
-server.get("/employee", (req: Request, res: Response) => {
-  console.log(req.url);
-  res.status(200).send("Hello my name is Nalin Govind V");
-});
-
-interface Profile {
-  age: number;
-  name: string;
-}
-
-interface Data {
-  profile: Profile;
-}
-
-server.get("/getData", (req: Request, res: Response) => {
-  let data: Data = {
-    profile: {
-      name: "Nalin Govind V",
-      age: 22,
-    },
-  };
-  console.log(data.profile.name);
-  res.status(200).send(data);
-});
-
-server.listen(3000, () => {
-  console.log("The server is running on port 3000");
-});
+(async () => {
+  try {
+    await dataSource.initialize();
+  } catch (e) {
+    console.log("Failed", e);
+    process.exit(1);
+  }
+  server.listen(3000, () => {
+    console.log("The server is running on port 3000");
+  });
+})();
